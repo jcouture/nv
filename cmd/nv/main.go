0edc172e3e1e43b3771c978f4be8e53b743d78ba
Jean-Philippe Couture <jcouture@gmail.com>
2021-05-24 09:26:58 -0400

    Track and display the build date and version.

--- cmd/nv/main.go
+++ cmd/nv/main.go
@@ -28,6 +28,7 @@ import (
 	"strings"
 	"syscall"
 
+	"github.com/jcouture/nv/internal/build"
 	"github.com/jcouture/nv/internal/parser"
 	"github.com/mitchellh/go-homedir"
 )
@@ -76,9 +77,10 @@ func main() {
 }
 
 func printUsage() {
-	usage := `nv — context specific environment variables
-Usage: nv <env files> <command> [arguments...]`
-	fmt.Println(usage)
+	usage := `nv %s(%s) — context specific environment variables
+Usage: nv <env files> <command> [arguments...]
+`
+	fmt.Printf(usage, build.Version, build.Date)
 }
 
 func setEnvVars(vars map[string]string) {
