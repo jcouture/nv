4abcd946c6465a09b6f1932638edda2226fca360
Jean-Philippe Couture <jcouture@gmail.com>
2026-01-23 15:56:50 -0500

    feat(cli): enrich version command output
      - derive git version/commit from build info with caching
      - switch text and json outputs to structured gitVersion-centric fields
      - add colored banner for version display and refresh command copy

--- internal/cli/version.go
+++ internal/cli/version.go
@@ -25,13 +25,114 @@ import (
 	"fmt"
 	"os"
 	"runtime"
+	"runtime/debug"
+	"strings"
+	"sync"
+	"text/tabwriter"
 
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
+)
+
+const unknown = "unknown"
+
+type versionInfo struct {
+	GitVersion string `json:"gitVersion"`
+	GitCommit  string `json:"gitCommit"`
+	GoVersion  string `json:"goVersion"`
+	Compiler   string `json:"compiler"`
+	Platform   string `json:"platform"`
+}
+
+var (
+	gitVersion = Version
+	gitCommit  = Commit
+
+	versionInfoOnce sync.Once
+	info            versionInfo
 )
 
 type versionOptions struct {
 	short  bool
 	format string
+}
+
+func getBuildInfo() *debug.BuildInfo {
+	bi, ok := debug.ReadBuildInfo()
+	if !ok {
+		return nil
+	}
+
+	return bi
+}
+
+func getGitVersion(bi *debug.BuildInfo) string {
+	if bi == nil {
+		return gitVersion
+	}
+
+	if bi.Main.Version == "(devel)" || bi.Main.Version == "" {
+		return gitVersion
+	}
+
+	return bi.Main.Version
+}
+
+func getCommit(bi *debug.BuildInfo) string {
+	return getKey(bi, "vcs.revision")
+}
+
+func getKey(bi *debug.BuildInfo, key string) string {
+	if bi == nil {
+		return unknown
+	}
+
+	for _, iter := range bi.Settings {
+		if iter.Key == key {
+			return iter.Value
+		}
+	}
+
+	return unknown
+}
+
+func getVersionInfo() versionInfo {
+	versionInfoOnce.Do(func() {
+		bi := getBuildInfo()
+
+		if gitVersion == "" || gitVersion == "dev" || gitVersion == unknown {
+			gitVersion = getGitVersion(bi)
+		}
+
+		if gitCommit == "" || gitCommit == "none" || gitCommit == unknown {
+			gitCommit = getCommit(bi)
+		}
+
+		info = versionInfo{
+			GitVersion: gitVersion,
+			GitCommit:  gitCommit,
+			GoVersion:  runtime.Version(),
+			Compiler:   runtime.Compiler,
+			Platform:   fmt.Sprintf("%s/%s", runtime.GOOS, runtime.GOARCH),
+		}
+	})
+
+	return info
+}
+
+func (i versionInfo) String() string {
+	var b strings.Builder
+	w := tabwriter.NewWriter(&b, 0, 0, 2, ' ', 0)
+
+	_, _ = fmt.Fprintf(w, "GitVersion:\t%s\n", i.GitVersion)
+	_, _ = fmt.Fprintf(w, "GitCommit:\t%s\n", i.GitCommit)
+	_, _ = fmt.Fprintf(w, "GoVersion:\t%s\n", i.GoVersion)
+	_, _ = fmt.Fprintf(w, "Compiler:\t%s\n", i.Compiler)
+	_, _ = fmt.Fprintf(w, "Platform:\t%s\n", i.Platform)
+
+	_ = w.Flush()
+
+	return b.String()
 }
 
 func newVersionCmd() *cobra.Command {
@@ -53,28 +154,50 @@ func newVersionCmd() *cobra.Command {
 }
 
 func runVersion(opts *versionOptions) error {
+	info := getVersionInfo()
+
 	if opts.short {
-		fmt.Fprintln(os.Stdout, Version)
+		fmt.Fprintln(os.Stdout, info.GitVersion)
 		return nil
 	}
 
 	switch opts.format {
 	case "json":
-		data := map[string]string{
-			"version": Version,
-			"commit":  Commit,
-			"built":   BuildDate,
-			"go":      runtime.Version(),
-		}
-		enc := json.NewEncoder(os.Stdout)
-		return enc.Encode(data)
+		return json.NewEncoder(os.Stdout).Encode(info)
 	case "text":
-		fmt.Fprintf(os.Stdout, "nv version %s\n", Version)
-		fmt.Fprintf(os.Stdout, "commit: %s\n", Commit)
-		fmt.Fprintf(os.Stdout, "built: %s\n", BuildDate)
-		fmt.Fprintf(os.Stdout, "go: %s\n", runtime.Version())
+		fmt.Fprintln(os.Stdout, banner())
+		fmt.Fprintln(os.Stdout)
+		fmt.Fprintln(os.Stdout, info.String())
 		return nil
 	default:
 		return fmt.Errorf("unknown format: %s", opts.format)
 	}
+}
+
+func banner() string {
+	lines := []string{
+		"_ ____   __",
+		"| '_ \\ \\ / /",
+		"| | | \\ V / ",
+		"|_| |_|\\_/",
+	}
+
+	palette := []color.Attribute{
+		color.FgHiRed,
+		color.FgHiMagenta,
+		color.FgHiBlue,
+		color.FgHiCyan,
+		color.FgHiGreen,
+		color.FgHiYellow,
+	}
+
+	colored := make([]string, 0, len(lines)+2)
+	colored = append(colored, color.New(color.FgHiWhite).Sprint("❯ nv version"))
+	for i, line := range lines {
+		c := color.New(palette[i%len(palette)])
+		colored = append(colored, c.Sprint(line))
+	}
+	colored = append(colored, color.New(color.FgHiWhite).Sprint("nv: Purpose-built env loader for your current task."))
+
+	return strings.Join(colored, "\n")
 }
